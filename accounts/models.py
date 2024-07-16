dbb4bb6181b0f889ad4dbc8839b7d80ee43616ef
amirreza <amir.rsf1380@gmail.com>
2024-07-17 01:54:18 +0330

    user model

--- accounts/models.py
+++ accounts/models.py
@@ -1,5 +1,12 @@
 from django.contrib.auth.models import AbstractUser
 
+from django_jalali.db import models as jmodels
+
+from phonenumber_field.modelfields import PhoneNumberField
+
 
 class CustomUser(AbstractUser):
-    pass
+    birth = jmodels.jDateField
+    phone_number = PhoneNumberField(blank=True, null=True)
+
+    objects = jmodels.jManager()
