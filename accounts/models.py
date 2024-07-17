48097d460f3b8158cf6e7528a7c64453d9a0dc40
amirreza <amir.rsf1380@gmail.com>
2024-07-18 00:11:34 +0330

    fixed superuser bug and made a field optional

--- accounts/models.py
+++ accounts/models.py
@@ -1,12 +1,16 @@
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractUser, UserManager
 
 from django_jalali.db import models as jmodels
 
 from phonenumber_field.modelfields import PhoneNumberField
 
 
+class CustomUserManager(UserManager, jmodels.jManager):
+    pass
+
+
 class CustomUser(AbstractUser):
-    birth = jmodels.jDateField()
+    birth = jmodels.jDateField(blank=True, null=True)
     phone_number = PhoneNumberField(blank=True, null=True)
 
-    objects = jmodels.jManager()
+    objects = CustomUserManager()
